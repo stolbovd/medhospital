c706fcd03f5110318009792fa87a6239b2317897
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-05-23 15:18:17 +0000

    Added commandHandlers, created eventSource for AttributeValue Aggregate roots.

--- trunk/backend/src/test/java/ua/com/alus/medhosp/backend/test/PatientAxonSpringTest.java
+++ trunk/backend/src/test/java/ua/com/alus/medhosp/backend/test/PatientAxonSpringTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import ua.com.alus.medhosp.backend.axon.api.patient.command.RemovePatientCommand;
+import ua.com.alus.medhosp.backend.axon.api.patient.command.SaveAttributeValueCommand;
 import ua.com.alus.medhosp.backend.axon.api.patient.command.SavePatientCommand;
 import ua.com.alus.medhosp.backend.domen.utils.UUID;
 import ua.com.alus.medhosp.backend.service.PatientService;
@@ -42,6 +43,16 @@ public class PatientAxonSpringTest extends AbstractTransactionalJUnit4SpringCont
         String uuid = UUID.uuid();
         commandBus.dispatch(new SavePatientCommand(uuid, null));
         commandBus.dispatch(new RemovePatientCommand(uuid, null));
+    }
+
+    @Test
+    public void saveAttributeValue(){
+        String entityId = "12C6A3D7-5EDD-4508-AD3D-C0CBAE131ABD";
+        String attributeId = "F9C308D7-8629-46F6-89DA-990DBE1E4CA8";
+        String value = "hello1";
+
+        //commandBus.dispatch(new SavePatientCommand(entityId,null));
+        commandBus.dispatch(new SaveAttributeValueCommand(entityId,null,attributeId, value));
     }
 
 }
