b273472bfa1b2d7ba46c60cd9072902eaa1dc02c
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-05-23 15:40:34 +0000

    Added commandHandlers, created eventSource for AttributeValue Aggregate roots.

--- backend/src/main/java/ua/com/alus/medhosp/backend/axon/api/eventsources/AttributeValueGenericFactory.java
+++ backend/src/main/java/ua/com/alus/medhosp/backend/axon/api/eventsources/AttributeValueGenericFactory.java
@@ -17,56 +17,18 @@ import java.lang.reflect.Constructor;
  */
 public class AttributeValueGenericFactory<T extends EventSourcedAggregateRoot> extends GenericAggregateFactory<T> {
 
-    private String aggregateType;
-    private Constructor<T> constructor;
-
-
-    /**
-     * Initialize the AggregateFactory for creating instances of the given <code>aggregateType</code>.
-     *
-     * @param aggregateType The type of aggregate this factory creates instances of.
-     * @throws IncompatibleAggregateException if the aggregate constructor throws an exception, or if the JVM security
-     *                                        settings prevent the GenericEventSourcingRepository from calling the
-     *                                        constructor.
-     */
     public AttributeValueGenericFactory(Class<T> aggregateType) {
         super(aggregateType);
-        Assert.isTrue(EventSourcedAggregateRoot.class.isAssignableFrom(aggregateType),
-                "The given aggregateType must be a subtype of EventSourcedAggregateRoot");
-        this.aggregateType = aggregateType.getSimpleName();
-        try {
-            this.constructor = aggregateType.getDeclaredConstructor(AggregateIdentifier.class);
-            if (!constructor.isAccessible()) {
-                constructor.setAccessible(true);
-            }
-        } catch (NoSuchMethodException e) {
-            throw new IncompatibleAggregateException(String.format(
-                    "The aggregate [%s] does not have a suitable constructor. "
-                            + "See Javadoc of GenericEventSourcingRepository for more information.",
-                    aggregateType.getSimpleName()), e);
-        }
     }
-
 
     @SuppressWarnings({"unchecked"})
     @Override
     public T createAggregate(AggregateIdentifier aggregateIdentifier, DomainEvent firstEvent) {
-        T aggregate;
-        if (AggregateSnapshot.class.isInstance(firstEvent)) {
-            aggregate = (T) ((AggregateSnapshot) firstEvent).getAggregate();
-        } else {
-            try {
-                aggregate = constructor.newInstance(aggregateIdentifier);
-            } catch (Exception e) {
-                throw new IncompatibleAggregateException(String.format(
-                        "The constructor [%s] threw an exception", constructor.toString()), e);
-            }
-        }
-        return aggregate;
+        return super.createAggregate(aggregateIdentifier, firstEvent);
     }
 
     @Override
     public String getTypeIdentifier() {
-        return aggregateType;
+        return super.getTypeIdentifier();
     }
 }
