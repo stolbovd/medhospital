d31bbc7c7a93e8c52e684caa79601ca50bc29ce6
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-14 13:57:29 +0000

    Added external library.

---
+++ trunk/sharedlib/src/main/java/ua/com/alus/medhosp/shared/data/Constants.java
@@ -0,0 +1,21 @@
+package ua.com.alus.medhosp.shared.data;
+
+/**
+ * This is class with main constants
+ *
+ * Created by Usatov Alexey
+ */
+public interface Constants {
+    String ERROR = "error";
+    String MESSAGE_ID = "messageId";
+    String RESULT = "result";
+
+    String KEY = "key";
+    String ENTITY_ID = "entityId";
+    String ATTRIBUTE_ID = "attributeId";
+    String ATTRIBUTE_TYPE = "attributeType";
+    String ATTRIBUTE_VALUE = "attributeValue";
+    String ATTRIBUTE_LABEL = "attributeLabel";
+    String CLASS = "class";
+    String SUPER_KEY_NAME = "superKeyName";
+}
