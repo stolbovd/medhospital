e363b4de8bbb229bb36d2299e5da7b3ee316d299
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-14 13:26:02 +0000

    currentChanges

--- trunk/frontend/src/main/java/ua/com/alus/medhosp/frontend/server/jms/JmsCommandListener.java
+++ trunk/frontend/src/main/java/ua/com/alus/medhosp/frontend/server/jms/JmsCommandListener.java
@@ -1,9 +1,16 @@
 package ua.com.alus.medhosp.frontend.server.jms;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+import ua.com.alus.medhosp.frontend.client.modules.patients.rpc.IPatientService;
+import ua.com.alus.medhosp.frontend.shared.AbstractDTO;
+import ua.com.alus.medhosp.frontend.shared.PatientAttributeValue;
+import ua.com.alus.medhosp.frontend.shared.PatientDTO;
 
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created Usatov Alexey
@@ -13,14 +20,78 @@ import javax.jms.MessageListener;
 public class JmsCommandListener implements MessageListener {
     private Logger logger = Logger.getLogger(JmsCommandListener.class);
 
+    private IPatientService patientService;
+
+    public IPatientService getPatientService() {
+        return patientService;
+    }
+
+    public void setPatientService(IPatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    private Map<String, String> command2Class = new HashMap<String, String>();
+
+    public void setCommand2Class(Map<String, String> command2class) {
+        this.command2Class = command2class;
+    }
+
+    public Map<String, String> getCommand2Class() {
+        return command2Class;
+    }
+
     public static final String COMMAND = "command";
+    public static final String CLASS = "class";
+
+    /*
+    The structure of message:
+    {
+    key1:value1,
+    key2:value2,
+    key3:value3
+    ...
+    }
+     */
 
     @SuppressWarnings("unchecked")
     public void onMessage(Message message) {
         try {
-            logger.info("onMessage in  FRONTEND!"+ message.getStringProperty(COMMAND));
+            logger.info("Recieved message:" + message.getStringProperty(COMMAND));
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> properties = mapper.readValue(message.getStringProperty(COMMAND), HashMap.class);
+            AbstractDTO object = createObject(properties.get(CLASS), properties);
+            executeUpdate(object);
         } catch (Exception e) {
             logger.trace(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private AbstractDTO createObject(String clazz, Map<String, String> properties) {
+        try {
+            if (getCommand2Class().get(clazz) == null) {
+                logger.trace(String.format("No class is associated for command: %s", clazz));
+                return null;
+            }
+            AbstractDTO resultObject = (AbstractDTO) Class.forName(getCommand2Class().get(clazz)).newInstance();
+            setProperties(resultObject, properties);
+            return resultObject;
+        } catch (Exception e) {
+            logger.trace(e);
+
+        }
+        return null;
+    }
+
+    private void setProperties(Map<String, String> object, Map<String, String> properties) {
+        object.putAll(properties);
+    }
+
+    public void executeUpdate(AbstractDTO object) {
+        if (object instanceof PatientDTO) {
+            getPatientService().createPatient((PatientDTO) object);
+        } else if (object instanceof PatientAttributeValue) {
+            getPatientService().savePatient((PatientAttributeValue) object, object.getColumns());
         }
     }
 
