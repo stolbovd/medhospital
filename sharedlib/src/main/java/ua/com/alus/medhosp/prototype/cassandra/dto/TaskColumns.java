2b283e24e614b8517d4d731c35e5b5ae664cf5c3
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-15 21:44:09 +0000

    Added Tasks

---
+++ sharedlib/src/main/java/ua/com/alus/medhosp/prototype/cassandra/dto/TaskColumns.java
@@ -0,0 +1,20 @@
+package ua.com.alus.medhosp.prototype.cassandra.dto;
+
+/**
+ * Columns for Task
+ * <p/>
+ * Created by Usatov Alexey
+ */
+public enum TaskColumns {
+    ENTITY_ID("entityId"), MESSAGE_ID("messageId"), MESSAGE_BODY("messageBody"), SUPER_KEY_NAME("superKeyName"), RESULT("result");
+
+    TaskColumns(String columnName) {
+        this.columnName = columnName;
+    }
+
+    private String columnName;
+
+    public String getColumnName() {
+        return columnName;
+    }
+}
