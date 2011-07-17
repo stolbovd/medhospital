45c3c5b729a0e2e780ca2b7ce38f0f7588020f3d
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-17 10:24:27 +0000

    Added Tasks

---
+++ sharedlib/src/main/java/ua/com/alus/medhosp/prototype/cassandra/goals/DtoGoal.java
@@ -0,0 +1,8 @@
+package ua.com.alus.medhosp.prototype.cassandra.goals;
+
+/**
+ * Commands for dto json from backend
+ */
+public enum DtoGoal {
+    SAVE, REMOVE
+}
--- sharedlib/src/main/java/ua/com/alus/medhosp/prototype/data/Constants.java
+++ sharedlib/src/main/java/ua/com/alus/medhosp/prototype/data/Constants.java
@@ -2,11 +2,12 @@ package ua.com.alus.medhosp.prototype.data;
 
 /**
  * This is class with main constants
- *
+ * <p/>
  * Created by Usatov Alexey
  */
 public interface Constants {
     String ERROR = "error";
+    String GOAL = "goal";
     String MESSAGE_ID = "messageId";
     String RESULT = "result";
 
