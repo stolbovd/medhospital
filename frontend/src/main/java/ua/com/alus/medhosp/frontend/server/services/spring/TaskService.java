180a605c67a1f79cb54ac6d49124b6d06e8e747b
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-18 10:58:20 +0000

    Removed FAKE_user_id.

--- frontend/src/main/java/ua/com/alus/medhosp/frontend/server/services/spring/TaskService.java
+++ frontend/src/main/java/ua/com/alus/medhosp/frontend/server/services/spring/TaskService.java
@@ -1,8 +1,11 @@
 package ua.com.alus.medhosp.frontend.server.services.spring;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import ua.com.alus.medhosp.frontend.client.modules.tasks.rpc.ITasksService;
 import ua.com.alus.medhosp.frontend.server.services.spring.dao.TaskDao;
 import ua.com.alus.medhosp.frontend.shared.TaskDTO;
+import ua.com.alus.medhosp.prototype.cassandra.dto.TaskColumns;
 
 import java.util.List;
 
@@ -21,18 +24,24 @@ public class TaskService implements ITasksService {
     }
 
     public void saveTask(TaskDTO taskDTO) {
+        taskDTO.put(TaskColumns.ENTITY_ID.getColumnName(), getUserId());
         getTaskDao().save(taskDTO);
     }
 
-    public List<TaskDTO> getTasks(String key) {
-        return getTaskDao().find(key, key);
+    public List<TaskDTO> getTasks() {
+        return getTaskDao().find(getUserId(), getUserId());
     }
 
     public void removeTask(String messageId) {
-        getTaskDao().removeSelectedSuperBySuperKeyName("FAKE_user_id", messageId);
+        getTaskDao().removeSelectedSuperBySuperKeyName(getUserId(), messageId);
     }
 
-    public TaskDTO findTask(String messageId){
-        return getTaskDao().find("FAKE_user_id", "FAKE_user_id", messageId).get(0);
+    public TaskDTO findTask(String messageId) {
+        return getTaskDao().find(getUserId(), getUserId(), messageId).get(0);
+    }
+
+    private String getUserId() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return user.getUsername();
     }
 }
