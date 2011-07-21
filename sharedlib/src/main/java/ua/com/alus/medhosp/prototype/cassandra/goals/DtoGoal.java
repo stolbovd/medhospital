e00c43f54b539ba075b3a53ef8440343def74538
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-21 13:23:01 +0000

    Current changes

--- sharedlib/src/main/java/ua/com/alus/medhosp/prototype/cassandra/goals/DtoGoal.java
+++ sharedlib/src/main/java/ua/com/alus/medhosp/prototype/cassandra/goals/DtoGoal.java
@@ -4,5 +4,5 @@ package ua.com.alus.medhosp.prototype.cassandra.goals;
  * Commands for dto json from backend
  */
 public enum DtoGoal {
-    SAVE, REMOVE
+    SAVE, REMOVE, GET
 }
--- sharedlib/src/main/java/ua/com/alus/medhosp/prototype/commands/Command.java
+++ sharedlib/src/main/java/ua/com/alus/medhosp/prototype/commands/Command.java
@@ -8,7 +8,7 @@ package ua.com.alus.medhosp.prototype.commands;
 public enum Command {
     SAVE_PATIENT("SavePatientCommand"), SAVE_ATTRIBUTE_VALUE("SaveAttributeValueCommand"),
     SAVE_ATTRIBUTE("SaveAttributeCommand"), REMOVE_PATIENT("RemovePatientCommand"),
-    REMOVE_ATTRIBUTE_VALUE("RemoveAttributeValueCommand");
+    REMOVE_ATTRIBUTE_VALUE("RemoveAttributeValueCommand"), RESEND_MESSAGE("ReSendMessageCommand");
 
     private String commandName;
     private Command(String commandName){
