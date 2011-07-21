4961ddabb7554e0bc8737a6ef94645d24c9a7d45
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-21 11:56:25 +0000

    Added MessageService.

---
+++ backend/src/main/java/ua/com/alus/medhosp/backend/domen/dao/message/MessageDao.java
@@ -0,0 +1,13 @@
+package ua.com.alus.medhosp.backend.domen.dao.message;
+
+import ua.com.alus.medhosp.backend.domen.dao.JpaDAO;
+import ua.com.alus.medhosp.backend.domen.entity.message.Message;
+
+/**
+ * MessageDAO
+ * <p/>
+ * Created by Usatov Alexey
+ */
+public class MessageDao extends JpaDAO<String, Message> {
+
+}
