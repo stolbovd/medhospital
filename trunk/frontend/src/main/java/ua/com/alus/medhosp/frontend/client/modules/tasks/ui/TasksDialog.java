d0f06bc8bf421368c9f88cb4fadd220c3cffbc22
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-25 13:58:55 +0000

    Moved Tasks Table

--- trunk/frontend/src/main/java/ua/com/alus/medhosp/frontend/client/modules/tasks/ui/TasksDialog.java
+++ trunk/frontend/src/main/java/ua/com/alus/medhosp/frontend/client/modules/tasks/ui/TasksDialog.java
@@ -1,6 +1,14 @@
 package ua.com.alus.medhosp.frontend.client.modules.tasks.ui;
 
 import com.smartgwt.client.widgets.Window;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.layout.VLayout;
+import ua.com.alus.medhosp.frontend.client.main.ui.ToolBarPanel;
+import ua.com.alus.medhosp.frontend.client.main.ui.ToolbarButton;
+import ua.com.alus.medhosp.frontend.client.resources.images.Icons;
+import ua.com.alus.medhosp.frontend.client.resources.locales.patients.PatientConstants;
+import ua.com.alus.medhosp.frontend.client.utils.ConstantsBundle;
 
 /**
  * Dialog for task table
@@ -10,6 +18,9 @@ import com.smartgwt.client.widgets.Window;
 public class TasksDialog extends Window {
 
     private TasksTable tasksTable;
+    private ToolBarPanel tasksToolbar;
+    PatientConstants constants = ConstantsBundle.getInstance().getPsConst();
+    Icons icons = ConstantsBundle.getInstance().getIcons();
 
     public TasksDialog() {
         super();
@@ -21,8 +32,31 @@ public class TasksDialog extends Window {
         setShowModalMask(true);
         centerInPage();
         setShowCloseButton(true);
-        setMembers(getTasksTable());
+        VLayout mainPanel = new VLayout();
+        mainPanel.setHeight100();
+        mainPanel.setMembers(getTasksToolbar(), getTasksTable());
+        addItem(mainPanel);
 
+    }
+
+    private ToolBarPanel getTasksToolbar() {
+        if (tasksToolbar == null) {
+            tasksToolbar = new ToolBarPanel();
+
+            final ToolbarButton refreshButton = new ToolbarButton(icons.refresh(),
+                    constants.getAllPatients());
+
+
+            refreshButton.addClickHandler(new ClickHandler() {
+                public void onClick(ClickEvent event) {
+                    getTasksTable().getController().refreshTable();
+                }
+            });
+
+            tasksToolbar.setMembers(refreshButton);
+            tasksToolbar.setHeight(30);
+        }
+        return tasksToolbar;
     }
 
     private TasksTable getTasksTable() {
