e239b3688b6f6c779e9c105f83b43965bf029ba9
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-26 21:20:07 +0000

    Improved basic functionality for creating attributes.

--- sharedlib/src/main/java/ua/com/alus/medhosp/prototype/cassandra/dto/Dto.java
+++ sharedlib/src/main/java/ua/com/alus/medhosp/prototype/cassandra/dto/Dto.java
@@ -4,7 +4,7 @@ package ua.com.alus.medhosp.prototype.cassandra.dto;
  * Dto enum for cassandra (client application)
  */
 public enum Dto {
-    PATIENT_ATTRIBUTE_VALUE("PatientAttributeValue"), PATIENT("PatientDTO");
+    PATIENT_ATTRIBUTE_VALUE("PatientAttributeValue"), PATIENT("PatientDTO"), ATTRIBUTE("AttributeDTO");
 
     Dto(String dtoName){
         this.dtoName = dtoName;
