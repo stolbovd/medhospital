f6816e3ea10e166dfbcbb0c64ce280316493be7d
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-07-26 18:05:56 +0000

    Current changes

---
+++ trunk/sharedlib/src/main/java/ua/com/alus/medhosp/prototype/cassandra/dto/AttributeColumns.java
@@ -0,0 +1,20 @@
+package ua.com.alus.medhosp.prototype.cassandra.dto;
+
+/**
+ * Attribute DTO columns
+ * <p/>
+ * Create by Usatov Alexey
+ */
+public enum AttributeColumns {
+    ENTITY_ID("entityId"), NAME("name");
+
+    AttributeColumns(String columnName) {
+        this.columnName = columnName;
+    }
+
+    private String columnName;
+
+    public String getColumnName() {
+        return columnName;
+    }
+}
