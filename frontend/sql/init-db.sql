354b42d8835a65d067a4277ef11f08169ad143f9
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-08-04 07:18:26 +0000

    Fixed comparators

--- frontend/sql/init-db.sql
+++ frontend/sql/init-db.sql
@@ -25,7 +25,7 @@ create column family Attributes with
 
 create column family TasksView with
   column_type='Super' and
-  comparator='BytesType' and
-  subcomparator='TimeUUIDType' and
+  comparator='TimeUUIDType' and
+  subcomparator='BytesType' and
   gc_grace=0
 ;
