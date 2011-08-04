c6e378ca39be112b402ae49a5079b7cf42aa6965
cunuukum@gmail.com <cunuukum@gmail.com@03a81441-71a0-1994-7bfd-8242377f7b7a>
2011-08-04 07:03:30 +0000

    Current changes.

---
+++ trunk/frontend/src/main/java/ua/com/alus/medhosp/frontend/client/modules/attributeValues/cto/AttributeValueCTO.java
@@ -0,0 +1,10 @@
+package ua.com.alus.medhosp.frontend.client.modules.attributeValues.cto;
+
+import ua.com.alus.medhosp.frontend.client.main.domen.ObjectCTO;
+import ua.com.alus.medhosp.frontend.shared.PatientAttributeValue;
+
+/**
+ * CTO
+ */
+public class AttributeValueCTO extends ObjectCTO<PatientAttributeValue, AttributeValueCTO> {
+}
